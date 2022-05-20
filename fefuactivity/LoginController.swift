25a7cd011bda94e5467f53e1a18fc20e5756c59f
mrrlanie <mrr.wrk@gmail.com>
2022-05-20 16:54:37 +1000

    First Homework
    //TO-DO: TextField class, constrainsts

---
+++ fefuactivity/LoginController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginController.swift
+//  fefuactivity
+//
+//  Created by students on 20.05.2022.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ fefuactivity/RegistrationControllerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegistrationController.swift
+//  fefuactivity
+//
+//  Created by students on 20.05.2022.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
