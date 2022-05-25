aeb03d36427c9ffc2a7c069f2ff7a1e63d4bcde1
mrrlanie <mrr.wrk@gmail.com>
2022-05-25 22:44:35 +1000

    Homework 2 is done

---
+++ fefuactivity/ActivityInformationController.swift
@@ -0,0 +1,29 @@
+//
+//  ActivityInformationController.swift
+//  fefuactivity
+//
+//  Created by students on 25.05.2022.
+//
+
+import UIKit
+
+class ActivityInformationController: UIViewController {
+    
+    var activityname = ""
+    var activitykm = ""
+    var activitylastedtime = ""
+    var activityduration = ""
+    
+    @IBOutlet var activityName: UILabel!
+    @IBOutlet var activityKm: UILabel!
+    @IBOutlet var activityLT: UILabel!
+    @IBOutlet var activityDuration: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activityName.text = activityname
+        activityKm.text = activitykm + " км"
+        activityLT.text = activitylastedtime + " часов"
+        activityDuration.text = activityduration + " часов назад"
+    }
+}
---
+++ fefuactivity/MyActivityViewController.swift
@@ -0,0 +1,78 @@
+//
+//  MyActivityViewController.swift
+//  fefuactivity
+//
+//  Created by students on 25.05.2022.
+//
+
+import UIKit
+
+class MyActivityViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet weak var emptyState: UIView!
+    
+    var currentkm: String = "0"
+    var currenttime: String = "0"
+    var currentname: String = "Название"
+    var currentlastedtime: String = "0"
+    let activitykm = ["15.4", "21"]
+    let activitydurations = ["4", "15"]
+    let activitynames = ["Велосипед", "Бег"]
+    let activitylastedtime = ["5", "0"]
+    var sectionnames = ["22.05.2022"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let nib = UINib(nibName: "ActivityCellViewTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "ActivityCellView")
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    @IBAction func activityButton(_ sender: UIButton){
+        emptyState.isHidden = true
+        tableView.isHidden = false
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(activitykm.count)
+        return activitykm.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ActivityCellView", for: indexPath) as! ActivityCell
+        cell.activityKM.text = activitykm[indexPath.row] + " км"
+        cell.activityDT.text = activitydurations[indexPath.row] + " часов"
+        cell.activityNM.text = activitynames[indexPath.row]
+        cell.activityLT.text = activitylastedtime[indexPath.row] + " часов назад"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let section = self.sectionnames[section]
+        return section
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionnames.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ShowActivity", sender: tableView.cellForRow(at: indexPath))
+        
+        currentname = activitynames[indexPath.row]
+        currenttime = activitydurations[indexPath.row]
+        currentkm = activitykm[indexPath.row]
+        currentlastedtime = activitylastedtime[indexPath.row]
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "ShowActivity" else { return }
+        guard let destination = segue.destination as? ActivityInformationController else {return}
+        destination.activityduration = String(currenttime)
+        destination.activityname = String(currentname)
+        destination.activitylastedtime = String(currentlastedtime)
+        destination.activitykm = String(currentkm)
+    }
+}
